2b6c2d8e575ebf6732a7ac6dbccf74b52b49e83c
Caio Silva <61994442+CaioSilva88@users.noreply.github.com>
2020-11-23 12:43:21 -0300

    Update soma.js

--- soma.js
+++ soma.js
@@ -19,7 +19,7 @@
     <script>
         
         var numero1 = Number(window.prompt('Digite o primeiro número a ser somado, por favor:'));
-        var numero2 = Number(window.prompt('Digite o primeiro número a ser somado, por favor:'));
+        var numero2 = Number(window.prompt('Digite o segundo número a ser somado, por favor:'));
         var resultado = numero1 + numero2;
         alert(`O resultado da soma é: ${resultado}`);
     </script>
